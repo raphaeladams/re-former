3dc81c8131ebb732a3f672ff573badbd4871702e
Raphael Adams <raphael.adams@shopify.com>
2020-11-20 15:00:11 -0500

    Set up Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "/graphql", to: "graphql#execute"
 end
